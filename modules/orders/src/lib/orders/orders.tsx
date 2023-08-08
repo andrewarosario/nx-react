cca71deeec8a72da2d2e8df37617da2876e26fb2
andrewarosario <andrew.arosario@gmail.com>
2023-08-08 16:56:23 -0300

    feat: add subtitle to orders page

--- modules/orders/src/lib/orders/orders.tsx
+++ modules/orders/src/lib/orders/orders.tsx
@@ -7,7 +7,8 @@ export interface OrdersProps {}
 export function Orders(props: OrdersProps) {
   return (
     <div className={styles['container']}>
-      <h1>Welcome to Orders!</h1>
+      <h1>Welcome to Orders!!</h1>
+      <h2>Visualize todos os produtos</h2>
       <ModulesSharedUi />
     </div>
   );
