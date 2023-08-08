8f82653bbbefe653412e7395c0353245031989dc
andrewarosario <andrew.arosario@gmail.com>
2023-08-08 16:52:09 -0300

    test: fix app router tests

--- modules/products/src/lib/product-list/product-list.tsx
+++ modules/products/src/lib/product-list/product-list.tsx
@@ -7,7 +7,7 @@ export interface ProductListProps {}
 export function ProductList(props: ProductListProps) {
   return (
     <div className={styles['container']}>
-      <h1>Welcome to ProductList!</h1>
+      <h1>Welcome to Product List!</h1>
       <ModulesSharedUi />
     </div>
   );
--- src/app/app.spec.tsx
+++ src/app/app.spec.tsx
@@ -1,15 +1,39 @@
 import { render } from '@testing-library/react';
 
+import { MemoryRouter } from 'react-router-dom';
 import App from './app';
 
 describe('App', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(<App />);
-    expect(baseElement).toBeTruthy();
+  it('should render home page', () => {
+    const { getByText } = render(
+      <MemoryRouter initialEntries={['/']}>
+        <App />
+      </MemoryRouter>
+    );
+
+    const homeText = getByText(/Home/i);
+    expect(homeText).toBeTruthy();
   });
 
-  it('should have a greeting as the title', () => {
-    const { getByText } = render(<App />);
-    expect(getByText(/Welcome my-app/gi)).toBeTruthy();
+  it('should render product list page', () => {
+    const { getByText } = render(
+      <MemoryRouter initialEntries={['/products']}>
+        <App />
+      </MemoryRouter>
+    );
+
+    const productListText = getByText(/Product List/i);
+    expect(productListText).toBeTruthy();
+  });
+
+  it('should render orders page', () => {
+    const { getByText } = render(
+      <MemoryRouter initialEntries={['/orders']}>
+        <App />
+      </MemoryRouter>
+    );
+
+    const ordersText = getByText(/Orders/i);
+    expect(ordersText).toBeTruthy();
   });
 });
